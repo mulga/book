f67b606d6a311d7772a148668de40fbd6f7f51a2
Mehmet Ulga <mehmetulga@gmail.com>
2022-10-14 09:49:40 +0200

    login and register page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>LoginPage</h1>
+  <div class="login_register_container">
+    <h3 class="text-sxl text-center mb-3">Login</h3>
+    <input type="text" placeholder="Username" class="input mb-3" />
+    <input type="password" placeholder="Password" class="input mb-3" />
+    <button class="default-button">Login</button>
+    <span class="text-center mt-3 text-sm"
+      >I'm new,
+      <a href="#" class="text-red-900 hover:text-black">Register</a>
+    </span>
   </div>
 </template>
 
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <h1>Register Page</h1>
+  <div class="login_register_container">
+    <h3 class="text-2xl text-center mb-3">Register</h3>
+    <input type="text" placeholder="Fullname" class="input mb-3" />
+    <input type="text" placeholder="Username" class="input mb-3" />
+    <input type="password" placeholder="Password" class="input mb-3" />
+    <button class="default-button">Register</button>
+    <span class="text-center mt-3 text-sm">
+      I'have already an account,
+      <a href="#" class="text-red-900 hover:text-black">Login</a>
+    </span>
   </div>
 </template>
 
